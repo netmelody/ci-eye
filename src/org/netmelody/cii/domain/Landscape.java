7bd00fb206bb2df1cb46c27d606b1ce65835a9f4
scarytom <t.denley@cantab.net>
2011-02-06 12:18:03 +0000

    Lend thoughts to configuration

---
+++ src/org/netmelody/cii/domain/Landscape.java
@@ -0,0 +1,14 @@
+package org.netmelody.cii.domain;
+
+public final class Landscape {
+
+    private final String name;
+    
+    public Landscape(String name) {
+        this.name = name;
+    }
+    
+    public String name() {
+        return name;
+    }
+}
