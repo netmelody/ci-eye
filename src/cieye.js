0a857dca93b0f4cde7629deaee60975879e0ab38
Tom Denley <tom.denley@youdevise.com>
2011-02-10 12:18:48 +0000

    refactored cieye.js to full oo safety

--- src/cieye.js
+++ src/cieye.js
@@ -1,28 +1,35 @@
 "use strict";
 
-function newBuildWidget(buildJson) {
+var ORG = (ORG) ? ORG : {};
+ORG.NETMELODY = {};
+
+ORG.NETMELODY.newBuildWidget = function(buildJson) {
     var buildDiv = $('<div></div>').addClass('progress-bar'),
         barDiv   = $('<div></div>');
         
     function updateProgress(percent) {
-        barDiv.setAttribute('style', 'width: ' + build.progress + '%');
+        barDiv.setAttribute('style', 'width: ' + percent + '%');
     }
     
     function refresh(newBuildJson) {
         updateProgress(newBuildJson.progress);
     }
     
-    buildDiv.appendChild(barDiv);
-    refresh(buildJson);
+    function initialise() {
+        buildDiv.appendChild(barDiv);
+        refresh(buildJson);
+    }
+    
+    initialise();
     
     return {
         updateFrom: refresh,
         getContent: function() { return buildDiv; },
         getId: function() { return "buildId"; }
     };
-}
+};
 
-function newTargetWidget(targetJson) {
+ORG.NETMELODY.newTargetWidget = function(targetJson) {
     var currentTargetJson = {},
         targetDiv = $('<div></div>').addClass('target'),
         buildsDiv = $('<div></div>');
@@ -38,12 +45,12 @@ function newTargetWidget(targetJson) {
         
         $(buildsDiv).empty();
         $.each(newTargetJson.builds, function(buildJson) {
-            buildsDiv.appendChild(newBuildWidget(buildJson).getContent());
+            buildsDiv.appendChild(ORG.NETMELODY.newBuildWidget(buildJson).getContent());
         });
     }
     
     function initialise() {
-        targetDiv.innerHTML = '<span>' + target.name + '</span>';
+        targetDiv.innerHTML = '<span>' + targetJson.name + '</span>';
         targetDiv.appendChild(buildsDiv);
         refresh(targetJson);
     }
@@ -55,40 +62,13 @@ function newTargetWidget(targetJson) {
         getContent: function() { return targetDiv; },
         getId: function() { return targetJson.id; }
     };
-}
+};
 
-function addBuild(targetDiv, build) {
-     var buildDiv = document.createElement('div'),
-         barDiv   = document.createElement('div');
-     
-     buildDiv.setAttribute('class', 'progress-bar');
-     barDiv.setAttribute('style', 'width: ' + build.progress + '%');
-     buildDiv.appendChild(barDiv);
-     targetDiv.appendChild(buildDiv);
-}
-
-function updateTarget(radiatorDiv, target) {
-    var targetDivId = 'target_' + target.id,
-        targetDiv = document.getElementById(targetDivId);
+ORG.NETMELODY.newRadiatorWidget = function() {
+    var radiatorDiv = $('<div></div>');
     
-    if (!targetDiv) {
-        targetDiv = document.createElement('div');
-        targetDiv.setAttribute('id', targetDivId);
-        targetDiv.innerHTML = '<span>' + target.name + '</span>';
-    }
-  
-    targetDiv.setAttribute('class', 'target ' + target.status);
-    $(targetDiv).children('.progress-bar').remove();
-    
-    for (i in target.builds) {
-        addBuild(targetDiv, target.builds[i]);
-    }
-    radiatorDiv.appendChild(targetDiv);
-}
-
-function refreshTargets(radiatorDiv) {
-    $.getJSON('landscapeobservation.json', { landscapeName: 'HIP' }, function(targetList) {
-        var targets = targetList.targets.sort(function(a, b) {
+    function refresh(targetGroupJson) {
+        var targets = targetGroupJson.targets.sort(function(a, b) {
             if (a.status === b.status) {
                 return (a.builds.length > b.builds.length) ? -1 : (a.builds.length < b.builds.length) ? 1 : 0;
             }
@@ -98,16 +78,37 @@ function refreshTargets(radiatorDiv) {
             return 1;
         });
         
-        for (i in targets) {
-            updateTarget(radiatorDiv, targets[i]);
-        }
-    });
-}
+        $(radiatorDiv).empty();
+        $.each(targets, function(targetJson) {
+            radiatorDiv.appendChild(ORG.NETMELODY.newTargetWidget(targetJson).getContent());
+        });
+    }
+    
+    return {
+        updateFrom: refresh,
+        getContent: function() { return radiatorDiv; }
+    };
+};
 
-function start() {
-    var radiatorDiv = document.getElementById('radiator');
-    refreshTargets(radiatorDiv);
-    setInterval(function(){ refreshTargets(radiatorDiv); }, 1000);
-}
+ORG.NETMELODY.newRadiator = function(radiatorDiv, repeatingTaskProvider) {
+    var radiatorWidget = ORG.NETMELODY.newRadiatorWidget();
+    
+    function refresh() {
+        $.getJSON('landscapeobservation.json', { landscapeName: 'HIP' }, function(targetList) {
+            radiatorWidget.updateFrom(targetList);
+        });
+    }
+    
+    function startup() {
+        radiatorDiv.appendChild(radiatorWidget.getContent());
+    
+        refresh();
+        repeatingTaskProvider.setInterval(refresh, 1000);
+    }
+    
+    return {
+        start: startup
+    };
+};
 
-window.onload = start;
+window.onload = ORG.NETMELODY.newRadiator(document.getElementById('radiator'), window).start();
