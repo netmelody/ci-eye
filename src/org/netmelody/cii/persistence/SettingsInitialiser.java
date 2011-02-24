5c55e3e964662bc36ceaceb3e77d6905467ed58e
Tom Denley <tom.denley@youdevise.com>
2011-02-24 17:02:40 +0000

    set up default settings file templates

---
+++ src/org/netmelody/cii/persistence/SettingsInitialiser.java
@@ -0,0 +1,54 @@
+package org.netmelody.cii.persistence;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.FileUtils;
+
+public final class SettingsInitialiser {
+    
+    private final File homeDir;
+    private final File viewsFile;
+    private final File picturesFile;
+    private final File picturesDir;
+
+    public SettingsInitialiser() {
+        this(new File(FileUtils.getUserDirectory(), ".ci-eye"));
+    }
+    
+    public SettingsInitialiser(File directory) {
+        this.homeDir = directory;
+        this.viewsFile = new File(homeDir, "views.txt");
+        this.picturesFile = new File(homeDir, "pictures.txt");
+        this.picturesDir = new File(homeDir, "pictures");
+        
+        try {
+            terraform();
+        } catch (IOException e) {
+            throw new IllegalStateException("Unable to initialise ci-eye server", e);
+        }
+    }
+
+    private void terraform() throws IOException {
+        if (!viewsFile.exists()) {
+            FileUtils.copyInputStreamToFile(resource("views.txt.template"), viewsFile);
+        }
+        
+        if (!picturesFile.exists()) {
+            FileUtils.copyInputStreamToFile(resource("pictures.txt.template"), picturesFile);
+        }
+        
+        if (!picturesDir.exists()) {
+            FileUtils.copyInputStreamToFile(resource("picture.png.template"), new File(picturesDir, "vlad.png"));
+        }
+    }
+
+    private InputStream resource(String named) {
+        return SettingsInitialiser.class.getClassLoader().getResourceAsStream(named);
+    }
+
+    public File viewsFile() {
+        return viewsFile;
+    }
+}
--- src/org/netmelody/cii/persistence/State.java
+++ src/org/netmelody/cii/persistence/State.java
@@ -1,54 +1,23 @@
 package org.netmelody.cii.persistence;
 
-import static com.google.common.collect.Lists.newArrayList;
-import static org.netmelody.cii.domain.CiServerType.DEMO;
-import static org.netmelody.cii.domain.CiServerType.JENKINS;
-import static org.netmelody.cii.domain.CiServerType.TEAMCITY;
-
-import java.io.File;
-
-import org.netmelody.cii.domain.Feature;
 import org.netmelody.cii.domain.Landscape;
 import org.netmelody.cii.domain.LandscapeGroup;
 
 public final class State {
 
-    private LandscapeGroup landscapes =
-        new LandscapeGroup(newArrayList(new Landscape("Ci-eye Demo", new Feature("My Product", "", DEMO)),
-                                        new Landscape("PubLive", new Feature("Main", "http://hudson.magnolia-cms.com", JENKINS)),
-                                        new Landscape("HIP", new Feature("HIP Hawk", "http://ccmain:8080", JENKINS),
-                                                             new Feature("HIP - Trunk", "http://teamcity-server:8111", TEAMCITY)),
-                                        new Landscape("HIP-HUDSON", new Feature("HIP Hawk", "http://ccmain:8080", JENKINS)),
-                                        new Landscape("HIP-TC", new Feature("HIP - Trunk", "http://teamcity-server:8111", TEAMCITY))));
+    private LandscapeGroup landscapes;
 
     public State() {
-        final String userHome = System.getProperty("user.home");
-        
-        if (null == userHome || userHome.isEmpty()) {
-            throw new IllegalStateException("Unable to get user's home directory.");
-        }
-        
-        final File settingsDir = new File(new File(userHome), ".ci-eye");
-        settingsDir.mkdir();
-        
-        final File viewsFile = new File(settingsDir, "views.txt");
-        landscapes = ViewsRepository.readLandscapesFrom(viewsFile);
-        
-//        final File picturesFile = new File(settingsDir, "pictures.json");
-//        try {
-//            viewsFile.createNewFile();
-//            picturesFile.createNewFile();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        
-//        final File picturesDir = new File(settingsDir, "pictures");
-//        picturesDir.mkdir();
+        SettingsInitialiser settings = new SettingsInitialiser();
+        landscapes = ViewsRepository.readLandscapesFrom(settings.viewsFile());
     }
-    
     
     public LandscapeGroup landscapes() {
         return this.landscapes;
+    }
+    
+    public Detective detective() {
+        return new Detective();
     }
 
     public void addLandscape(Landscape landscape) {
