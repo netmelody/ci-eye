f109d7aaab935c887e10acfad31f706b002553b0
Tom Denley <tom.denley@youdevise.com>
2011-03-02 17:46:14 +0000

    fix analysis of comments on last bad build

--- src/org/netmelody/cii/witness/jenkins/jsondomain/JobDetail.java
+++ src/org/netmelody/cii/witness/jenkins/jsondomain/JobDetail.java
@@ -1,6 +1,16 @@
 package org.netmelody.cii.witness.jenkins.jsondomain;
 
+import static com.google.common.base.Predicates.notNull;
+import static com.google.common.collect.Collections2.filter;
+import static com.google.common.collect.Collections2.transform;
+import static com.google.common.collect.Lists.newArrayList;
+
+import java.util.Collection;
 import java.util.List;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Ordering;
 
 public final class JobDetail extends Job {
     public String displayName;
@@ -27,17 +37,39 @@ public final class JobDetail extends Job {
     //queueItem
     
     public String lastBadBuildUrl() {
-        if (null == lastUnstableBuild) {
-            if (null == lastFailedBuild) {
-                return lastFailedBuild.url;
-            }
+        final List<Build> lastBadBuilds = newArrayList(lastUnstableBuild, lastUnsuccessfulBuild, lastFailedBuild);
+        final Collection<String> candidates = filter(filter(transform(filter(lastBadBuilds, notNull()), toUrl()), notNull()), notEmpty());
+        
+        if (candidates.isEmpty()) {
             return url;
         }
         
-        if (null == lastFailedBuild) {
-            return lastUnstableBuild.url;
-        }
-        
-        return (lastUnstableBuild.number >= lastFailedBuild.number) ? lastUnstableBuild.url : lastFailedBuild.url;
+        return lexographicallyLastFrom(candidates);
+    }
+    
+    private String lexographicallyLastFrom(final Collection<String> candidates) {
+        return new Ordering<String>() {
+            @Override public int compare(String left, String right) {
+                return left.compareTo(right);
+            }
+        }.max(candidates);
+    }
+    
+    private Predicate<String> notEmpty() {
+        return new Predicate<String>() {
+            @Override
+            public boolean apply(String input) {
+                return input.length() > 0;
+            }
+        };
+    }
+    
+    private Function<Build, String> toUrl() {
+        return new Function<Build, String>() {
+            @Override
+            public String apply(Build input) {
+                return input.url;
+            }
+        };
     }
 }
