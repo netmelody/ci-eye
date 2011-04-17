5395a9950ec2ae4f6e853cee61ffa2594089d14f
scarytom <t.denley@cantab.net>
2011-04-17 10:11:53 +0100

    thinking about versioning

--- src/org/netmelody/cieye/server/configuration/State.java
+++ src/org/netmelody/cieye/server/configuration/State.java
@@ -1,8 +1,12 @@
 package org.netmelody.cieye.server.configuration;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.InetAddress;
+import java.util.jar.Manifest;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.netmelody.cieye.core.domain.Landscape;
 import org.netmelody.cieye.core.domain.LandscapeGroup;
 import org.netmelody.cieye.core.observation.KnownOffendersDirectory;
@@ -12,6 +16,8 @@ import org.netmelody.cieye.server.PictureFetcher;
 
 public final class State implements LandscapeFetcher, PictureFetcher, ConfigurationFetcher {
 
+    private static final Log LOG = LogFactory.getLog(State.class);
+    
     private final SettingsInitialiser settings = new SettingsInitialiser();
     private final KnownOffendersDirectory detective = new RecordedKnownOffenders(settings.picturesFile());
     private LandscapeGroup landscapes = new ViewsRepository(settings.viewsFile()).landscapes();
@@ -48,5 +54,16 @@ public final class State implements LandscapeFetcher, PictureFetcher, Configurat
         catch (Exception e) {
             return "";
         }
+    }
+    
+    public String getVersion() {
+        String result = "";
+        try {
+            final Manifest manifest = new Manifest(State.class.getResourceAsStream("/META-INF/MANIFEST.MF"));
+            result = manifest.getMainAttributes().getValue("Implementation-Version");
+        } catch (IOException e) {
+            LOG.warn("Failed to read Implementation-Version", e);
+        }
+        return result;
     }
 }
