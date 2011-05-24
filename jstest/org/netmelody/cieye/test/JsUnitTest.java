e928be0da4e847d7f3878a0c325e7168aa5c5a77
Tom Denley <tom.denley@youdevise.com>
2011-05-24 10:10:48 +0100

    upgrade to menodora 0.0.3

--- jstest/org/netmelody/cieye/test/JsUnitTest.java
+++ jstest/org/netmelody/cieye/test/JsUnitTest.java
@@ -5,10 +5,10 @@ import org.netmelody.menodora.JasmineJavascriptContext;
 import org.netmelody.menodora.JasmineSuite;
 
 @RunWith(JasmineSuite.class)
-@JasmineJavascriptContext(source={"/Player.js", "/Song.js"},
-                          jasmineHelpers={"/SpecHelper.js"},
-                          jasmineSpecs={"/PlayerSpec.js"},
-                          withSimulatedDom=true)
+@JasmineJavascriptContext(source={"Player.js", "Song.js"},
+                          jasmineHelpers={"SpecHelper.js"},
+                          jasmineSpecs={"*Spec.js"},
+                          withSimulatedDom=false)
 public final class JsUnitTest {
     
 }
