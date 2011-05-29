c2f988c926b2265b406e50a0d3add4dcb5987506
scarytom <t.denley@cantab.net>
2011-05-29 10:45:33 +0100

    remove dead files

--- jstest/TestRunner.js
+++
@@ -1,3 +0,0 @@
-load("env.js-1.2/env.rhino.1.2.js");
-Envjs.scriptTypes["text/javascript"] = true;
-window.location = "file:///c:/Users/tom/development/eclipseworkspace/ci-eye/jstest/TestRunner.html";
