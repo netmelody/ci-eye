4327fa8b8acd19055801429725f1cae2843f09b2
scarytom <t.denley@cantab.net>
2011-06-16 22:12:55 +0100

    complete unit test for the settings initialiser

--- test/org/netmelody/cieye/server/configuration/test/SettingsInitialiserTest.java
+++ test/org/netmelody/cieye/server/configuration/test/SettingsInitialiserTest.java
@@ -1,6 +1,12 @@
 package org.netmelody.cieye.server.configuration.test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.is;
+import static org.netmelody.menodora.wrapped.org.apache.commons.io.filefilter.TrueFileFilter.TRUE;
+
 import java.io.File;
+import java.util.Arrays;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,5 +22,25 @@ public final class SettingsInitialiserTest {
     createsConfigurationFiles() {
         final File folder = testFolder.newFolder("test");
         new SettingsInitialiser(folder);
+        
+        final String[] generatedFiles = folder.list(TRUE);
+        Arrays.sort(generatedFiles);
+        assertThat(generatedFiles, is(arrayContaining("logging.properties",
+                                                      "logs",
+                                                      "pictures",
+                                                      "pictures.txt",
+                                                      "views.txt")));
+    }
+    
+    @Test public void
+    generatesDemoPictureFiles() {
+        final File folder = testFolder.newFolder("test");
+        new SettingsInitialiser(folder);
+        
+        final String[] generatedFiles = new File(folder, "pictures").list(TRUE);
+        Arrays.sort(generatedFiles);
+        assertThat(generatedFiles, is(arrayContaining("doh.png",
+                                                      "stupid.png",
+                                                      "vlad.png")));
     }
 }
