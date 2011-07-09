c2b31af8df638ed32504f3eb61b2110588dc7950
scarytom <t.denley@cantab.net>
2011-07-09 23:59:01 +0100

    yet more functional

--- src/org/netmelody/cieye/server/configuration/RecordedObservationTargets.java
+++ src/org/netmelody/cieye/server/configuration/RecordedObservationTargets.java
@@ -1,6 +1,11 @@
 package org.netmelody.cieye.server.configuration;
 
-import java.util.ArrayList;
+import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Iterables.skip;
+import static com.google.common.collect.Iterables.transform;
+import static com.google.common.collect.Lists.newArrayList;
+import static org.netmelody.cieye.core.utility.Irritables.partition;
+
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,12 +18,6 @@ import org.netmelody.cieye.server.LandscapeFetcher;
 
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
-
-import static com.google.common.collect.Iterables.filter;
-import static com.google.common.collect.Iterables.skip;
-import static com.google.common.collect.Iterables.transform;
-import static com.google.common.collect.Lists.newArrayList;
-import static org.netmelody.cieye.core.utility.Irritables.partition;
 
 public final class RecordedObservationTargets implements LandscapeFetcher, Refreshable {
 
@@ -82,18 +81,32 @@ public final class RecordedObservationTargets implements LandscapeFetcher, Refre
                 }
                 
                 final String landscapeName = matcher.group(1);
-                final Iterable<String> featuresNames = filter(skip(data, 1), notBlank());
-                
-                final List<Feature> features = new ArrayList<Feature>();
-                for (String featurename : featuresNames) {
-                    Matcher featureMatcher = FEATURE_REGEX.matcher(featurename);
-                    if (featureMatcher.matches()) {
-                        features.add(new Feature(featureMatcher.group(3),
-                                                 featureMatcher.group(2),
-                                                 new CiServerType(featureMatcher.group(1))));
-                    }
-                }
+                final List<Feature> features = newArrayList(transform(filter(transform(filter(skip(data, 1), notBlank()), toMatchers()), matches()), toFeature()));
                 return new Landscape(landscapeName, features.toArray(new Feature[features.size()]));
+            }
+        };
+    }
+
+    private static Function<String, Matcher> toMatchers() {
+        return new Function<String, Matcher>() {
+            @Override public Matcher apply(String featureLine) {
+                return FEATURE_REGEX.matcher(featureLine);
+            }
+        };
+    }
+    
+    private static Predicate<Matcher> matches() {
+        return new Predicate<Matcher>() {
+            @Override public boolean apply(Matcher matcher) {
+                return matcher.matches();
+            }
+        };
+    }
+    
+    private static Function<Matcher, Feature> toFeature() {
+        return new Function<Matcher, Feature>() {
+            @Override public Feature apply(Matcher featureMatcher) {
+                return new Feature(featureMatcher.group(3), featureMatcher.group(2), new CiServerType(featureMatcher.group(1)));
             }
         };
     }
