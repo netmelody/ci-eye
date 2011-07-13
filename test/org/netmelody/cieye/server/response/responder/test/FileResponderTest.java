2ba513dd0ac615de1a8591ff998491c478b42801
scarytom <t.denley@cantab.net>
2011-07-13 19:54:18 +0100

    include content testcase in FileResponderTest

--- test/org/netmelody/cieye/server/response/responder/test/FileResponderTest.java
+++ test/org/netmelody/cieye/server/response/responder/test/FileResponderTest.java
@@ -1,6 +1,11 @@
 package org.netmelody.cieye.server.response.responder.test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
@@ -28,5 +33,20 @@ public class FileResponderTest {
         responder.writeTo(response);
         context.assertIsSatisfied();
     }
-
+    
+    @Test public void
+    writesTheCorrectContent() throws IOException {
+        final FileResponder responder = new FileResponder("org/netmelody/cieye/server/response/responder/test/bob.js");
+        final OutputStream outputStream = new ByteArrayOutputStream();
+        final Response response = context.mock(Response.class);
+        context.checking(new Expectations() {{
+            allowing(response).getOutputStream(); will(returnValue(outputStream));
+            
+            ignoring(response).set(with(any(String.class)), with(any(String.class)));
+        }});
+        
+        responder.writeTo(response);
+        
+        assertThat(outputStream.toString(), is("//hi"));
+    }
 }
