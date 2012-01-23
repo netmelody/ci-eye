97df75dd117731de498da50b543484df8e6817df
Tom Denley <tom.denley@youdevise.com>
2012-01-23 09:24:12 +0000

    improve flyout menu -- less distracting

--- src/main/webapp/resources/jquery.flymenu.js
+++ src/main/webapp/resources/jquery.flymenu.js
@@ -13,35 +13,70 @@
                 "border": "1px solid #000000",
                 "font-family": "verdana,sans-serif",
                 "font-size": "0.8em"
+            },
+            "menuTabStyle": {
+                "position": "absolute",
+                "backgroundColor": "#ffffff",
+                "opacity": "1",
+                "top": "0px",
+                "right": "0px",
+                "zIndex": "499",
+                "width": "5px",
+                "height": "5px",
+                "color": "#000000",
+                "padding": "3px",
+                "border": "0px",
+                "font-family": "verdana,sans-serif",
+                "font-size": "0.8em"
             }
         },
-        menuTab = $("<div></div>").hide().css(style.menuStyle),
+        menuTab = $("<div></div>").hide().css(style.menuTabStyle),
         menu = $("<div></div>").hide().css(style.menuStyle),
         mouseover = false,
-        timeoutId = undefined;
-    
+        menuTabTimeoutId = undefined,
+        menuTimeoutId = undefined;
+
+    function showMenuTab() {
+        if (menu.is(":visible")) {
+            return;
+        }
+        
+        menuTab.show();
+        if (menuTabTimeoutId) {
+            window.clearTimeout(menuTabTimeoutId);
+            menuTabTimeoutId = undefined;
+        }
+        menuTabTimeoutId = window.setTimeout(hideMenuTab, 500);
+    }
+
+    function hideMenuTab() {
+        menuTab.hide();
+    }
+
     function hideMenu() {
         if (!mouseover) {
             menu.animate({ "top": "-50px" }, 100, function() { menu.hide(); });
         }
     }
-    
+
     function showMenu() {
-        if (timeoutId) {
-            window.clearTimeout(timeoutId);
-            timeoutId = undefined;
+        hideMenuTab();
+        
+        if (menuTimeoutId) {
+            window.clearTimeout(menuTimeoutId);
+            menuTimeoutId = undefined;
         }
         if (!menu.is(":visible")) {
             menu.show();
             menu.animate({ "top": "0px" }, 200);
         }
-        timeoutId = window.setTimeout(hideMenu, 500);
+        menuTimeoutId = window.setTimeout(hideMenu, 500);
     }
-    
+
     function elementFor(id, label, initialState, changeHandler) {
         var ele = $("<input type='checkbox'/>").attr("id", id).attr("name", id),
             labelEle = $("<label></label>").attr("for", id).text(label);
-        
+
         ele.attr("checked", initialState);
         ele.bind("change", function() { changeHandler(ele.prop("checked")); });
         
@@ -56,22 +91,28 @@
             elementFor("flymenu-" + index, entry.label, entry.initialState, entry.changeHandler);
         });
     }
-    
+
     $(document).ready(function() {
         menu.appendTo("body");
+        menuTab.appendTo("body");
+
+        menuTab.bind("mouseenter", function() { showMenu(); });
+
+        menu.bind("mouseenter", function(){ mouseover = true; });
+        menu.bind("mouseleave", function(){
+            mouseover = false;
+            window.setTimeout(hideMenu, 500);
+        });
     });
-    
+
     $.fn.flyMenu = function(entries) {
         buildMenuFor(entries);
+
         $(this).bind("mousemove", function(event) {
-            if (event.pageY < 200 && event.pageX > ($("body").width() - 200)) {
-                showMenu();
-                return false;
-            }
-            return true;
+            showMenuTab();
+            return false;
         });
-        menu.bind("mouseenter", function(){ mouseover = true; });
-        menu.bind("mouseleave", function(){ mouseover = false; });
+
         return this;
     };
 })(jQuery);
