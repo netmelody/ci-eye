71b9296530b8eef857ed623a05d2ea117d65f77f
scarytom <t.denley@cantab.net>
2012-03-19 22:52:00 +0000

    add tests for tc7 api changes

--- src/test/java/org/netmelody/cieye/spies/teamcity/test/TeamCityCommunicatorTest.java
+++ src/test/java/org/netmelody/cieye/spies/teamcity/test/TeamCityCommunicatorTest.java
@@ -7,6 +7,7 @@ import org.hamcrest.Matchers;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.netmelody.cieye.core.observation.CommunicationNetwork;
 import org.netmelody.cieye.spies.StubContact;
@@ -57,6 +58,33 @@ public final class TeamCityCommunicatorTest {
         
         contact.respondingWith("http://foo/app/rest/changes/id:2", "tc_6.5.5_changes_2.json");
 
+        final List<Change> changes = communicator.changesOf(buildDetail);
+        assertThat(changes, is(Matchers.<Change>iterableWithSize(2)));
+    }
+
+    @Ignore("pending implementation")
+    @Test public void
+    requestsSingularBuildChangesForTeamCitySevenApi() {
+        final BuildDetail buildDetail = new BuildDetail();
+        buildDetail.changes = new ChangesHref();
+        buildDetail.changes.count = 1;
+        buildDetail.changes.href = "/app/rest/changes/id:1";
+        
+        contact.respondingWith("http://foo/app/rest/changes/id:1", "tc_7.0.0_changes_1.json");
+        
+        final List<Change> changes = communicator.changesOf(buildDetail);
+        assertThat(changes, is(Matchers.<Change>iterableWithSize(1)));
+    }
+
+    @Test public void
+    requestsMultipleBuildChangesForTeamCitySevenApi() {
+        final BuildDetail buildDetail = new BuildDetail();
+        buildDetail.changes = new ChangesHref();
+        buildDetail.changes.count = 2;
+        buildDetail.changes.href = "/app/rest/changes/id:2";
+        
+        contact.respondingWith("http://foo/app/rest/changes/id:2", "tc_7.0.0_changes_2.json");
+        
         final List<Change> changes = communicator.changesOf(buildDetail);
         assertThat(changes, is(Matchers.<Change>iterableWithSize(2)));
     }
