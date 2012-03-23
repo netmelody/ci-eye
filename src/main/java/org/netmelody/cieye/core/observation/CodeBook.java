adac872c0ee75f29f5fb7652bc3ae4d5cf51df1c
Tom Denley <tom.denley@youdevise.com>
2012-03-23 12:10:04 +0000

    add user/pass to CodeBook

--- src/main/java/org/netmelody/cieye/core/observation/CodeBook.java
+++ src/main/java/org/netmelody/cieye/core/observation/CodeBook.java
@@ -8,8 +8,12 @@ import com.google.common.base.Functions;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.JsonDeserializer;
 
+import static com.google.common.base.Functions.compose;
+
 public final class CodeBook {
 
+    private final String username;
+    private final String password;
     private final SimpleDateFormat dateFormat;
     private final ImmutableMap<Class<?>, JsonDeserializer<?>> deserialisers;
     private final Function<String, String> munger;
@@ -19,21 +23,35 @@ public final class CodeBook {
     }
 
     public CodeBook(SimpleDateFormat dateFormat) {
-        this(dateFormat, Functions.<String>identity(), ImmutableMap.<Class<?>, JsonDeserializer<?>>of());
+        this("", "", dateFormat, Functions.<String>identity(), ImmutableMap.<Class<?>, JsonDeserializer<?>>of());
     }
 
-    private CodeBook(SimpleDateFormat dateFormat, Function<String, String> munger, ImmutableMap<Class<?>, JsonDeserializer<?>> deserialisers) {
+    private CodeBook(String username, String password, SimpleDateFormat dateFormat, Function<String, String> munger, ImmutableMap<Class<?>, JsonDeserializer<?>> deserialisers) {
+        this.username = username;
+        this.password = password;
         this.dateFormat = dateFormat;
         this.deserialisers = deserialisers;
         this.munger = munger;
     }
 
-    public <T> CodeBook withJsonDeserializerFor(Class<T> type, JsonDeserializer<T> deserialiser) {
-        return new CodeBook(this.dateFormat, this.munger, extend(this.deserialisers, type, deserialiser));
+    public CodeBook withCredentials(String name, String pass) {
+        return new CodeBook(name, pass, this.dateFormat, this.munger, this.deserialisers);
     }
 
     public CodeBook withRawContentMunger(Function<String, String> munger) {
-        return new CodeBook(this.dateFormat, Functions.compose(munger, this.munger), this.deserialisers);
+        return new CodeBook(this.username, this.password, this.dateFormat, compose(munger, this.munger), this.deserialisers);
+    }
+
+    public <T> CodeBook withJsonDeserializerFor(Class<T> type, JsonDeserializer<T> deserialiser) {
+        return new CodeBook(this.username, this.password, this.dateFormat, this.munger, extend(this.deserialisers, type, deserialiser));
+    }
+
+    public String username() {
+        return username;
+    }
+
+    public String password() {
+        return password;
     }
 
     public SimpleDateFormat dateFormat() {
