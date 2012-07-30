546e0f0edcf9da8a879e775e00b6d2370e711464
Tom Denley <tom.denley@youdevise.com>
2012-07-30 13:36:27 +0100

    only display all-green squirrel if there are actually some green builds... no builds at all does not count -- fixes #14

--- src/main/webapp/resources/cieye.js
+++ src/main/webapp/resources/cieye.js
@@ -293,7 +293,7 @@ ORG.NETMELODY.CIEYE.newRadiatorWidget = function() {
             delete targetWidgets[index];
         });
         
-        if (radiatorDiv.children().length >= 0 && radiatorDiv.children().filter("div:visible").length === 0) {
+        if (radiatorDiv.children(".target").length > 0 && radiatorDiv.children().filter("div:visible").length === 0) {
             allGreenImg.width("100%");
             allGreenImg.height("100%");
             allGreenImg.show();
