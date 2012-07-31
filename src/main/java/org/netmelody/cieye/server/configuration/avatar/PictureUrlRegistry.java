4478d8230bf6e9f6346a7d1fec2f37222395ce71
Dmitry Sidorenko <rustydragon@gmail.com>
2012-08-01 03:01:04 +0400

    Refactoring in preparation for additional avatar providers

---
+++ src/main/java/org/netmelody/cieye/server/configuration/avatar/PictureUrlRegistry.java
@@ -0,0 +1,37 @@
+package org.netmelody.cieye.server.configuration.avatar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class PictureUrlRegistry {
+  private       List<PictureUrlProvider> providers;
+  private final LocalImage               defaultProvider;
+
+  public PictureUrlRegistry() {
+    this.providers = new ArrayList<PictureUrlProvider>();
+    providers.add(new Gravatar());
+    defaultProvider = new LocalImage();
+    providers.add(defaultProvider);
+  }
+
+  public PictureUrlProvider providerFor(String prefix) {
+    for (PictureUrlProvider provider : providers) {
+      if (provider.handlesPrefix().equals(prefix)) {
+        return provider;
+      }
+    }
+    return defaultProvider;
+  }
+
+  public String getPictureUrl(final String image) {
+    String[] imageParts = image.split(":");
+    PictureUrlProvider provider;
+    if (imageParts.length == 1) {
+      provider = defaultProvider;
+      return provider.imageUrlFor(image);
+    } else {
+      provider = providerFor(imageParts[0]);
+      return provider.imageUrlFor(imageParts[1]);
+    }
+  }
+}
