6885b1369210a23c554910210f694d2a58349e75
scarytom <t.denley@cantab.net>
2012-08-23 21:30:26 +0100

    restore original gravatar testcase

--- src/test/java/org/netmelody/cieye/server/configuration/test/RecordedKnownOffendersTest.java
+++ src/test/java/org/netmelody/cieye/server/configuration/test/RecordedKnownOffendersTest.java
@@ -16,6 +16,7 @@ import org.junit.rules.TemporaryFolder;
 import org.netmelody.cieye.core.domain.Sponsor;
 import org.netmelody.cieye.server.configuration.RecordedKnownOffenders;
 import org.netmelody.cieye.server.configuration.SettingsFile;
+import org.netmelody.cieye.server.configuration.avatar.Gravatar;
 
 
 public final class RecordedKnownOffendersTest {
@@ -79,6 +80,7 @@ public final class RecordedKnownOffendersTest {
     
     @Test public void
     looksUpOffenderAndProvidesLinkToGravatarMugshot() {
-        assertThat(offenders.search("john").size(), is(1));
+        final String gravatarUrlWithHashedEmail = new Gravatar().imageUrlFor("john.doe@gmail.com");
+        assertThat(offenders.search("john"), contains(new Sponsor("", gravatarUrlWithHashedEmail)));
     }
 }
