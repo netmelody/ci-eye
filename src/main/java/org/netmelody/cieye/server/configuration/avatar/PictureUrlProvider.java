e8e86bcab024db70295f4a827ddcd9ca7a136c90
scarytom <t.denley@cantab.net>
2012-08-23 22:26:03 +0100

    simplify PictureUrlRegistry

--- src/main/java/org/netmelody/cieye/server/configuration/avatar/PictureUrlProvider.java
+++ src/main/java/org/netmelody/cieye/server/configuration/avatar/PictureUrlProvider.java
@@ -1,7 +1,16 @@
 package org.netmelody.cieye.server.configuration.avatar;
 
+import com.google.common.base.Function;
+
 public interface PictureUrlProvider {
+    
     String imageUrlFor(String image);
 
     String handlesPrefix();
+
+    public static Function<PictureUrlProvider, String> TO_PREFIX = new Function<PictureUrlProvider, String>() {
+        @Override public String apply(PictureUrlProvider provider) {
+            return provider.handlesPrefix();
+        }
+    };
 }
--- src/main/java/org/netmelody/cieye/server/configuration/avatar/PictureUrlRegistry.java
+++ src/main/java/org/netmelody/cieye/server/configuration/avatar/PictureUrlRegistry.java
@@ -1,39 +1,25 @@
 package org.netmelody.cieye.server.configuration.avatar;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableList;
+
+import static com.google.common.collect.Maps.uniqueIndex;
 
 public final class PictureUrlRegistry {
-    private final List<PictureUrlProvider> providers;
-    private final LocalImage defaultProvider;
+    
+    private final Map<String, PictureUrlProvider> providers = uniqueIndex(ImmutableList.of(new Gravatar(),
+                                                                                           new Robohash(),
+                                                                                           new SimpleHttp(),
+                                                                                           new LocalImage()),
+                                                                          PictureUrlProvider.TO_PREFIX);
 
-    public PictureUrlRegistry() {
-        this.providers = new ArrayList<PictureUrlProvider>();
-        providers.add(new Gravatar());
-        providers.add(new Robohash());
-        providers.add(new SimpleHttp());
-        defaultProvider = new LocalImage();
-        providers.add(defaultProvider);
-    }
-
-    public PictureUrlProvider providerFor(String prefix) {
-        for (PictureUrlProvider provider : providers) {
-            if (provider.handlesPrefix().equals(prefix)) {
-                return provider;
-            }
-        }
-        return defaultProvider;
+    private String urlFor(String prefix, String suffix) {
+        return providers.get(prefix).imageUrlFor(suffix);
     }
 
     public String getPictureUrl(final String image) {
-        String[] imageParts = image.split(":");
-        PictureUrlProvider provider;
-        if (imageParts.length == 1) {
-            provider = defaultProvider;
-            return provider.imageUrlFor(image);
-        } else {
-            provider = providerFor(imageParts[0]);
-            return provider.imageUrlFor(imageParts[1]);
-        }
+        final String[] parts = image.split(":", 2);
+        return (parts.length == 1) ? urlFor("", image) : urlFor(parts[0], parts[1]);
     }
 }
