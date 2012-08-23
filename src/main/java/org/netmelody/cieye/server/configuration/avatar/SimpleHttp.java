884a9d4ab1d7328090222faa58873c7a87eeb493
scarytom <t.denley@cantab.net>
2012-08-23 22:26:45 +0100

    simplify http

--- src/main/java/org/netmelody/cieye/server/configuration/avatar/SimpleHttp.java
+++ src/main/java/org/netmelody/cieye/server/configuration/avatar/SimpleHttp.java
@@ -1,12 +1,11 @@
 package org.netmelody.cieye.server.configuration.avatar;
 
-import java.text.MessageFormat;
 
 public final class SimpleHttp implements PictureUrlProvider {
 
     @Override
     public String imageUrlFor(String picture) {
-        return MessageFormat.format("http:{0}", picture);
+        return "http:" + picture;
     }
 
     @Override
